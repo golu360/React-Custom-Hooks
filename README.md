74e87a0a0bd49ce9e2611fab5661461426f665a4
Rishabh-Kodes <rishabhkodes@gmail.com>
2021-10-04 12:21:12 +0530

    Add useHover

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # React-Custom-Hooks
+
 A repository containing custom React.js hooks.
 
 [Contribution Guidelines](https://github.com/golu360/React-Custom-Hooks/blob/main/contributing.md)
-
-
 
 #### Contents:
 
 - [`useToggle`](https://codesandbox.io/s/bold-kirch-sji4m)
 - [`useOnline`](https://codesandbox.io/s/blazing-cdn-5w0e3)
 - [`useLocalStorage`](https://codesandbox.io/s/xenodochial-chatelet-ww02b)
+- [`useHover`](https://codesandbox.io/s/usehover-0d15s)
---
+++ src/hooks/useHover/useHover.js
@@ -0,0 +1,25 @@
+import { useState, useEffect, useRef } from "react";
+
+const useHover = () => {
+  const ref = useRef(null);
+  const [value, setValue] = useState(false);
+
+  const handleMouseOver = () => setValue(true);
+  const handleMouseOut = () => setValue(false);
+
+  useEffect(() => {
+    const node = ref.current;
+    if (node) {
+      node.addEventListener("mouseover", handleMouseOver);
+      node.addEventListener("mouseout", handleMouseOut);
+      return () => {
+        node.removeEventListener("mouseover", handleMouseOver);
+        node.removeEventListener("mouseout", handleMouseOut);
+      };
+    }
+  }, [ref.current]);
+
+  return [ref, value];
+};
+
+export default useHover;
